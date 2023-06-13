1ebc875402c5acb73075dfc91ff8e5154d1be836
vanm34MV <vanm34@vse.cz>
2023-06-13 18:58:16 +0200

    filter done

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,95 +1,129 @@
 <!DOCTYPE html>
 <html>
 <!-- content -->
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Reality Check</title>
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <title>Reality Check</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <!-- Our Custom CSS -->
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
-    <!-- Font Awesome JS -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+  <!-- Font Awesome JS -->
+  <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-lg">
+  <nav class="navbar navbar-expand-lg">
     <div class="container-fluid ms-auto">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ms-auto">
-            <a class="nav-link" href="{{ route('login') }}">Sign in</a>
-            <a class="nav-link" href="{{ route('register') }}">Sign up</a>
+          <a class="nav-link" href="{{ route('login') }}">Sign in</a>
+          <a class="nav-link" href="{{ route('register') }}">Sign up</a>
         </div>
-        </div>
+      </div>
     </div>
-</nav>
+  </nav>
 
-<div class="wrapper">
+  <div class="wrapper">
     <!-- Sidebar -->
     <nav id="sidebar">
       <div class="sidebar-header">
-            <h3>Reality Check</h3>
+        <h3>Reality Check</h3>
       </div>
 
-        <ul class="list-unstyled components">
-            {{-- <p>Dummy Heading</p> --}}
-            <li>
-                <a href="#home">Home</a>
-            </li>
-            <li>
-                <!-- tady routa na nemovitost -->
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
+      <ul class="list-unstyled components">
+        {{-- <p>Dummy Heading</p> --}}
+        <li>
+          <a href="{{ url('/') }}">Home</a>
+        </li>
+        <li>
+          <a href="{{ url('/properties') }}">View Properties</a>
+
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
     </nav>
 
     <!-- Page Content -->
     <div id="content">
       <div class="MainPage">Welcome</div>
-      <img class="MainPage" src="{{ asset('img/house.jpg') }}" height="15%"  width="100%">
-      
-      
+      <img class="MainPage" src="{{ asset('img/house.jpg') }}" height="15%" width="100%">
+
+
       <div class="MainPageText">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis. Proin pede metus, vulputate nec, fermentum fringilla
         , vehicula vitae, justo. Et harum quidem rerum facilis est et expedita distinctio. Donec quis nibh at felis congue commodo. Nulla accumsan, elit sit amet varius semper, nulla mauris mol
         lis quam, tempor suscipit diam nulla vel leo. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Sed ac dolor sit amet purus malesuada congue. Etiam posuere lac
-        us quis dolor. Aliquam erat volutpat. 
+        us quis dolor. Aliquam erat volutpat.
       </div>
+
       <div class="circle-container">
-        {{-- Aparments --}}
+        <!-- Apartment -->
         <div class="circle">
-          <h3>apps</h3>
+          <h3>{{ $numberOfApartments }}</h3>
           <p>Bytů</p>
           <div class="sub-circles">
             <div class="sub-circle">
-              <h4>dsa</h4>
+              <h4>{{ $numberOfApartmentsForRent }}</h4>
               <p>K pronájmu</p>
             </div>
             <div class="sub-circle">
-              <h4>dsa</h4>
+              <h4>{{ $numberOfApartmentsForSale }}</h4>
               <p>Na prodej</p>
             </div>
           </div>
         </div>
-        
-      </div>   
+
+        <!-- House -->
+        <div class="circle">
+          <h3>{{ $numberOfHouses }}</h3>
+          <p>Bytů</p>
+          <div class="sub-circles">
+            <div class="sub-circle">
+              <h4>{{ $numberOfHousesForRent }}</h4>
+              <p>K pronájmu</p>
+            </div>
+            <div class="sub-circle">
+              <h4>{{ $numberOfHousesForSale }}</h4>
+              <p>Na prodej</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Lots -->
+        <div class="circle">
+          <h3>{{ $numberOfLots }}</h3>
+          <p>Bytů</p>
+          <div class="sub-circles">
+            <div class="sub-circle">
+              <h4>{{ $numberOfLotsForRent }}</h4>
+              <p>K pronájmu</p>
+            </div>
+            <div class="sub-circle">
+              <h4>{{ $numberOfLotsForSale }}</h4>
+              <p>Na prodej</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>    
+  </div>
+  </div>
 </body>
 
 <footer>
-  <!-- jQuery CDN - Slim version (=without AJAX) -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <!-- Popper.JS -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <!-- Bootstrap JS -->
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 </footer>
+
 </html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PropertyController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PropertyListController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +22,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', [PropertyController::class, 'getProperties']);
+
+// Route::get('/properties', [PropertyListController::class, 'index']);
+
+// Route::get('/properties', function () {
+//     return view('properties');
+// });
+
+Route::get('/homePage', function () {
+    return view('homePage');
+});
+
+Route::get('/properties', [PropertyListController::class, 'index'])->name('propertiesList.index');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
+    // pokud chci, aby byl klient přihlášen, aby se na tyto stránky mohl dostat
+
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
